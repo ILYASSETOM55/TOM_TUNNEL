9d8d41d2be3b3134550fc1b2fc838d25075f1d91
ILYASSETOM55 <jt3615900@gmail.com>
2026-09-11 19:35:01 +0100

    Update xray.sh

--- core/xray.sh
+++ core/xray.sh
@@ -1,5 +1,5 @@
 clear
-export SERVER_HOST="https://raw.githubusercontent.com/RootNexTPro/nexTPro-ScriptAll/main"
+export SERVER_HOST="https://raw.githubusercontent.com/ILYASSETOM55/TOM_TUNNEL/main"
 export DEBIAN_FRONTEND=noninteractive
 log() { printf "%b
 " "[INFO] $*"; }
