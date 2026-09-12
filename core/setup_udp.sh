76eb23a3d4684fc5a7708351985f2aba7abccd96
ILYASSETOM55 <jt3615900@gmail.com>
2026-09-12 16:42:59 +0100

    Update setup_udp.sh

--- core/setup_udp.sh
+++ core/setup_udp.sh
@@ -1,5 +1,5 @@
 clear
-export SERVER_HOST="https://raw.githubusercontent.com/RootNexTPro/nexTPro-ScriptAll/main"
+export SERVER_HOST="https://raw.githubusercontent.com/ILYASSETOM55/TOM_TUNNEL/main"
 export UDP_DIR="/etc/udp-custom"
 export SERVICE_FILE="/etc/systemd/system/udp-custom.service"
 update_system() {
