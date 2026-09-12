de39a6c7b27a507f12d026548c2a80f43b36f7ec
ILYASSETOM55 <jt3615900@gmail.com>
2026-09-12 22:25:36 +0100

    Update tom_tunnel_bot.py

--- tom_tunnel_core_bot/tom_tunnel_bot.py
+++ tom_tunnel_core_bot/tom_tunnel_bot.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(levelname)s %(m
 
 CONFIG_FILE = '/etc/nexus_bot/config.json'
 
-MENU_IMAGE_URL = "https://github.com/user-attachments/assets/baf681b1-820a-428d-85cf-703c25ff498b"
+MENU_IMAGE_URL = "https://github.com/user-attachments/assets/df687a92-7883-403d-8dab-0b62a86326eb"
 
 def load_config():
     if not os.path.exists(CONFIG_FILE): return None
